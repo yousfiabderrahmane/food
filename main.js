ad40fb889f339067fd513c94490ae63ba8d2f2f9
Yousfi Abderrahmane <bdrrhmnyousfi@gmail.com>
2022-10-17 11:51:22 +0100

    dev : adding behavior

---
+++ main.js
@@ -0,0 +1,29 @@
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll(".container ul li");
+const button = document.getElementById("btn");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  //   console.log(pageYOffset); // how much we scrolled
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 80 - 160;
+    console.log(sectionTop); //calculate from top to section //160 = padding
+    const sectionHeight = section.clientHeight; //section height
+    if (pageYOffset >= sectionTop) {
+      current = section.getAttribute("id");
+      setTimeout(() => {
+        section.classList.add("show");
+      }, 100);
+      if (current === "about") {
+        button.style.animation = "slide 1s linear forwards";
+      }
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(`${current}z`)) {
+      li.classList.add("active");
+    }
+  });
+});
