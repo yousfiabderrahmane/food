0b6f3b8e18784efdff37e7a18e614c07dffc7105
Yousfi Abderrahmane <bdrrhmnyousfi@gmail.com>
2022-10-18 09:45:28 +0100

    dev : scroll bar event

--- main.js
+++ main.js
@@ -77,3 +77,13 @@ if (window.matchMedia("(max-width: 700px)").matches) {
     hamburgerBtn.style.display = "inline-block";
   });
 }
+//progress bar
+const filled = document.querySelector(".filled");
+
+function update() {
+  filled.style.width = `${
+    (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100
+  }%`;
+  requestAnimationFrame(update);
+}
+update();
